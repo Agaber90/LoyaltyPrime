d7a3a8f4a79a5c94201ed00ff80c0825891f6cae
Ahmed <agaber.eltaweel@gmail.com>
2021-03-07 01:31:14 +0300

    LoyaltyPrimeAddCoreIntialCommit

---
+++ LoyaltyPrime/LoyaltyPrime.Data/Enities/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LoyaltyPrime.Data.Enities
+{
+    public class Member : IEntity
+    {
+        [Key]
+        public long ID { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ LoyaltyPrime/LoyaltyPrime.Data/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoyaltyPrime.Data
+{
+    public interface IEntity
+    {
+        long ID { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime UpdatedDate { get; set; }
+    }
+}
